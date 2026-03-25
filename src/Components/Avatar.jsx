a262e2207a920146a4b4cd4d2f952d8ff6ea41d8
Fabian2023 <fasalpe34@gmail.com>
2026-03-25 11:24:47 -0500

    avances pruebas

--- src/Components/Avatar.jsx
+++ src/Components/Avatar.jsx
@@ -6,22 +6,28 @@ const Avatar = () => {
   const location = useLocation();
   const name = location.state?.name;
 
+  const [eventouchKey, setEventouchKey] = useState(0);
+
   // 🔹 VIDEO SALUDO
   const [videoUrl, setVideoUrl] = useState("");
 
   // 🔹 VIDEO EVENTOUCH (nuevo)
   const [eventouchVideoUrl, setEventouchVideoUrl] = useState("");
 
-  const [loading, setLoading] = useState(true);
+  // 🔥 NUEVO: control intro
+  const [showIntro, setShowIntro] = useState(true);
+  const introVideo =
+    "https://videointroevent.s3.us-east-1.amazonaws.com/videointro2.mp4";
 
   // 🔥 traer video Eventouch
   const handleEventouchVideo = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:5000/api/eventouch-video"
+        "http://localhost:5000/api/eventouch-video",
       );
 
       setEventouchVideoUrl(response.data.videoUrl);
+      setEventouchKey(prev => prev + 1);
     } catch (error) {
       console.error(error);
     }
@@ -32,14 +38,12 @@ const Avatar = () => {
       try {
         const response = await axios.post(
           "http://localhost:5000/api/generate-greeting",
-          { name }
+          { name },
         );
 
         setVideoUrl(response.data.videoUrl);
       } catch (error) {
         console.error(error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -48,45 +52,74 @@ const Avatar = () => {
 
   return (
     <div className="w-full min-h-screen relative flex flex-col items-center justify-start text-white px-4 py-6">
+      {/* 🔥 IMAGEN FONDO (NUEVO) */}
+      <img
+        src="/bg.png"
+        alt="avatar"
+        className="
+          absolute 
+          w-146
+          h-218
+          max-w-sm
+          md:max-w-lg
+          lg:max-w-4xl 
+          
+          
+        "
+      />
 
-      {/* LOADER SOLO PARA SALUDO */}
-      {loading && (
-        <h2 className="text-xl md:text-3xl animate-pulse mb-6 text-center">
-          Preparando tu saludo...
-        </h2>
+      {/* 🔥 VIDEO INTRO (NUEVO) */}
+      {showIntro && (
+        <video
+          src={introVideo}
+          autoPlay
+          onEnded={() => setShowIntro(false)}
+          className="
+          relative z-10
+          w-full
+          max-w-sm
+          md:max-w-lg
+          lg:max-w-4xl
+          -mt-87.5
+          scale-95
+          "
+        />
       )}
 
       {/* 🔹 VIDEO SALUDO */}
-      {!eventouchVideoUrl && videoUrl && (
+      {!showIntro && !eventouchVideoUrl && videoUrl && (
         <video
           key={videoUrl}
           src={videoUrl}
           autoPlay
           className="
+          relative z-10
           w-full
           max-w-sm
           md:max-w-lg
           lg:max-w-xl
           rounded-xl
+          mt-2
           
-          mb-10
+          
           "
         />
       )}
 
       {/* 🔥 VIDEO EVENTOUCH (MISMO ESTILO) */}
-      {eventouchVideoUrl && (
+      {!showIntro && eventouchVideoUrl && (
         <video
-          key={eventouchVideoUrl}
+          key={eventouchKey}
           src={eventouchVideoUrl}
           autoPlay
           className="
+          relative z-10
           w-full
           max-w-sm
           md:max-w-lg
           lg:max-w-4xl
-          rounded-xl
-          -mt-87.5
+          -mt-90
+          scale-95
           
           "
         />
@@ -94,26 +127,24 @@ const Avatar = () => {
 
       {/* PREGUNTAS */}
       <div className=" absolute flex flex-col gap-4 w-full max-w-md md:max-w-2xl lg:max-w-3xl mt-237.5">
-
         <div
           onClick={handleEventouchVideo}
-          className="bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold "
+          className=" z-10 bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold "
         >
           ¿Qué es Eventouch?
         </div>
 
-        <div className="bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
+        <div className=" z-10 bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
           ¿Qué experiencias ofrecen?
         </div>
 
-        <div className="bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
+        <div className=" z-10 bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
           ¿Cómo funcionan los tótems?
         </div>
 
-        <div className="bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
+        <div className=" z-10 bg-[#753E89] hover:bg-purple-700 transition p-6 md:p-8 rounded-xl cursor-pointer text-center text-lg md:text-3xl lg:text-4xl font-semibold">
           ¿Puedo participar en una trivia?
         </div>
-
       </div>
     </div>
   );
